306919d27f5af7c17556a42a87a642c3e2feafd9
daniloesk <47666332+daniloesk@users.noreply.github.com>
2020-12-10 12:06:00 -0300

    npx create-single-spa --moduleType root-config --dir root --layout --orgName myorg --packageManager npm --typescript

---
+++ root/webpack.config.js
@@ -0,0 +1,40 @@
+const webpackMerge = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-ts");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = (webpackConfigEnv, argv) => {
+  const orgName = "myorg";
+  const defaultConfig = singleSpaDefaults({
+    orgName,
+    projectName: "root-config",
+    webpackConfigEnv,
+    argv,
+  });
+
+  const merge = webpackMerge({
+    customizeArray: webpackMerge.unique(
+      "plugins",
+      ["HtmlWebpackPlugin"],
+      (plugin) => plugin.constructor && plugin.constructor.name
+    ),
+  });
+
+  return merge(
+    {
+      plugins: [
+        new HtmlWebpackPlugin({
+          inject: false,
+          template: "src/index.ejs",
+          templateParameters: {
+            isLocal: webpackConfigEnv && webpackConfigEnv.isLocal === "true",
+            orgName,
+          },
+        }),
+      ],
+    },
+    defaultConfig,
+    {
+      // modify the webpack config however you'd like to by adding to this object
+    }
+  );
+};
