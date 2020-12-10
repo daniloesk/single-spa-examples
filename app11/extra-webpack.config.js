8755421ff7ce3d74f7618fa1e58782751b45363e
daniloesk <47666332+daniloesk@users.noreply.github.com>
2020-12-10 14:04:19 -0300

    Dynamic public path for lazy loading

--- app11/extra-webpack.config.js
+++ app11/extra-webpack.config.js
@@ -1,7 +1,13 @@
 const singleSpaAngularWebpack = require('single-spa-angular/lib/webpack').default;
+const SystemJSPublicPathWebpackPlugin = require("systemjs-webpack-interop/SystemJSPublicPathWebpackPlugin");
 
 module.exports = (config, options) => {
   const singleSpaWebpackConfig = singleSpaAngularWebpack(config, options);
+
+  singleSpaWebpackConfig.plugins.push(new SystemJSPublicPathWebpackPlugin({
+    // ONLY NEEDED FOR WEBPACK 1-4. Not necessary for webpack@5
+    systemjsModuleName: "@myorg/app11"
+  }));
 
   // Feel free to modify this webpack config however you'd like to
   return singleSpaWebpackConfig;
