5434d091bc36d966de03d47394c3019b2809faaf
daniloesk <47666332+daniloesk@users.noreply.github.com>
2020-12-10 13:10:44 -0300

    single-spa-angular routing

--- app11/src/app/app-routing.module.ts
+++ app11/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
+import { APP_BASE_HREF } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  /*
+   * Makes sure we don't try to show a 404 ou error while single-spa is transitioning between routes
+   * https://single-spa.js.org/docs/ecosystem-angular#configure-routes
+   */
+  { path: '**', component: EmptyRouteComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    /*
+     * Should be same as mount in root, but have strange effects when navigate between apps.
+     * https://single-spa.js.org/docs/ecosystem-angular#configure-routes
+     */
+    { provide: APP_BASE_HREF, useValue: '/' },
+  ],
 })
 export class AppRoutingModule { }
--- app11/src/app/app.module.ts
+++ app11/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmptyRouteComponent,
   ],
   imports: [
     BrowserModule,
