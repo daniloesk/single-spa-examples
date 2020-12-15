d33fd22397bb31655e02485aadbf8f5cfaf21aa6
daniloesk <47666332+daniloesk@users.noreply.github.com>
2020-12-15 06:13:35 -0300

    Use cors

--- proxy/app.js
+++ proxy/app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -8,6 +9,7 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
